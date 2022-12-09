2f9b285a4d7e54d191877001a5c8c4fd0d170446
Ditto <yanzhiking@126.com>
2022-12-09 14:59:34 +0800

    子应用注入主应用的store 和 component

--- src/views/userManage.vue
+++ src/views/userManage.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
     I am userManage
+    globle token: {{ this.$store.state.globeData.token || 'none' }}
+    <!-- 主应用下发的loading -->
+    <Loading></Loading>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  created() {
+    console.log(this.$store.state)
+  },
+  methods: {},
+}
+</script>
