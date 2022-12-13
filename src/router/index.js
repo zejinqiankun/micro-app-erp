9a0307fa968ed7db373a6808ea76d991517a5ca0
suyc <1232dev@ht.com>
2022-12-13 11:37:36 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/userManage.vue')
+  },
+  {
+    path: '/*',
+    name: '404',
+    component: () => import('../views/errorPage/404.vue'),
+    meta: {
+      notRequireAuth: true
+    }
   }
 ]
 
